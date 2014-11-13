8bba77afa65eb7ba73bfcc393f9708b75b91040e
longnight <longnight@gmail.com>
2014-11-13 14:13:13 +0800

    kk

--- flaskr.py
+++ flaskr.py
@@ -1,13 +1,23 @@
 #coding=utf-8
 
+
 # all the imports
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 
+
 # configuration
-DATABASE = '/tmp/flaskr.db'
+DATABASE = './flaskr.db'
 DEBUG = True
-SECRET_KEY = 'development key'
+SECRET_KEY = 'development key fdsasfasfasas'
 USERNAME = 'admin'
 PASSWORD = 'default'
+
+
+# create our little application :)
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
