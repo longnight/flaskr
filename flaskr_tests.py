18e5f9f5e3f5cc08d60327b00e4f9f54696b96b3
longnight <longnight@gmail.com>
2014-11-13 15:21:30 +0800

    more test method

--- flaskr_tests.py
+++ flaskr_tests.py
@@ -15,5 +15,18 @@ class FlaskrTestCase(unittest.TestCase):
         os.close(self.db_fd)
         os.unlink(flaskr.app.config['DATABASE'])
 
+    def test_empty_db(self):
+        rv = self.app.get('/')
+        assert 'No entries here so far' in rv.data
+
+    def login(self, username, password):
+        return self.app.post('/login', data=dict(
+            username=username,
+            password=password
+            ), follow_redirects=True)
+
+    def logout(self):
+        return self.app.get('/logout', follow_redirects=True)
+
 if __name__ == '__main__':
     unittest.main()
