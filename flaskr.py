393878bbc291eca6e7a8b103570254b7f05c6810
longnight <longnight@gmail.com>
2014-11-13 14:27:42 +0800

    after db connected, first view routine.

--- flaskr.py
+++ flaskr.py
@@ -3,6 +3,8 @@
 
 # all the imports
 import sqlite3
+from contextlib import closing
+
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 
@@ -14,10 +16,39 @@ SECRET_KEY = 'development key fdsasfasfasas'
 USERNAME = 'admin'
 PASSWORD = 'default'
 
-
 # create our little application :)
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+
+@app.route('/')
+def show_entries():
+    cur = g.db.execute('select title, text from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template('show_entries.html', entries=entries)
+
+
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
